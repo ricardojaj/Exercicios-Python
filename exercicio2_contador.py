bc448da9f2410d6c20f7d266dcaed1bec0e0a479
ricardojaj <rickanjos85@gmail.com>
2025-09-28 13:53:53 -0300

    Create: script exercicio 2

---
+++ exercicio2_contador.py
@@ -0,0 +1,18 @@
+#Exercicio 1 - Contador
+#Crie uma função que peça um número N e conte de 1 até N.
+#Use for para imprimir os números.
+#Use uma variável para guardar o número informado.
+#Se o número for menor que 1, mostre uma mensagem de erro.
+
+def contador():
+    try:
+        numero = int(input("Digite um numero inteiro: "))
+        i = 1
+        for i in range(i, numero + 1):
+            print(i)
+
+    except ValueError:
+        print("Numero digitado é invalido!")
+
+
+contador()
